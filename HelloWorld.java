530f42c3234edfee5d4790cb85bfc9a2d892db6c
Aidan <aidanjricks@gmail.com>
2020-08-20 16:44:25 +0000

    art

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.print("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii---------------------------------iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii----------------------------------------------iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii----------iiiii------------------------iiiii-----------iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii------------iiiii------------------------iiiii--------------iiiiiiiiiiiiiiiiiiiiiiiiiiii------------------------------------------------------------iiiiiiiiiiiiiiiiiiiiiiiiiiii------------------------------------------------------------iiiiiiiiiiiiiiiiiiiiiiiiiiii----------------ii--------------------------ii--------------iiiiiiiiiiiiiiiiiiiiiiiiiiiiii---------------iiiiiii--------------iiiiiii-------------iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii------------------iiiiiiiiiiiiiiii----------------iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii------------------------------------iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+
+
+  }
+
+
+} 
